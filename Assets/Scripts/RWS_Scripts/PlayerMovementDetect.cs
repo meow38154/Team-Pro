abbdbcb42ced18e1c3d063a4f95b538421b96e74
rrwwss <kaiser090220@naver.com>
2025-04-10 23:27:03 +0900

    feat : blockplayer

--- Assets/Scripts/RWS_Scripts/PlayerMovementDetect.cs
+++ Assets/Scripts/RWS_Scripts/PlayerMovementDetect.cs
@@ -6,6 +6,7 @@ public class PlayerMovementDetect : MonoBehaviour
     [SerializeField] private SpriteRenderer _rend;
     [SerializeField] private GameObject _player;
     [SerializeField] private string _playerName = "RWS_Player";
+    [SerializeField] private bool _isBlocked;
 
     private float _playerX;
     private float _playerY;
@@ -13,22 +14,31 @@ public class PlayerMovementDetect : MonoBehaviour
     private void Awake()
     {
         _player = GameObject.Find(_playerName);
+        _rend = GetComponent<SpriteRenderer>();
     }
 
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
     public void OnClick()
     {
-        Vector3 newPosition = _player.transform.position;
-        newPosition += transform.localPosition;
-        _player.transform.position = newPosition;
+        if (_isBlocked == false)
+        {
+            Vector3 newPosition = _player.transform.position;
+            newPosition += transform.localPosition;
+            _player.transform.position = newPosition;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Block"))
+        {
+            _isBlocked = true;
+            _rend.color = Color.red;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _isBlocked = false;
+        _rend.color = Color.green;
     }
 }
