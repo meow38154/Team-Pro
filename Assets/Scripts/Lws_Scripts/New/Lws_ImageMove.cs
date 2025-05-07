dd8f7063dd9dd15c0a836f564b17c27899f9a2b5
riwoo0902 <imliu4342@gmail.com>
2025-05-07 20:00:19 +0900

    feat:ImageMove
    haha

--- Assets/Scripts/Lws_Scripts/New/Lws_ImageMove.cs
+++ Assets/Scripts/Lws_Scripts/New/Lws_ImageMove.cs
@@ -3,25 +3,38 @@ using System.Collections;
 
 public class Lws_ImageMove : MonoBehaviour
 {
-    GameObject _parent;
-    Vector3 _parentPos;
+    [SerializeField] GameObject _traget;
+    [SerializeField] float _moveSpeed = 3f;
+    Vector3 _tragetPos;
     Vector3 _myPos;
     Vector3 _moveVec;
+    bool _moveing = false;
     void Start()
     {
-        _parent = transform.parent.gameObject;
-        _parentPos = _parent.transform.position;
-        transform.position = _parentPos;
+        _tragetPos = _traget.transform.position;
+        transform.position = _tragetPos;
         _myPos = transform.position;
     }
 
     void Update()
     {
-
-
-
+        _myPos = transform.position;
+        _tragetPos = _traget.transform.position;
+        if (_tragetPos != _myPos)
+        {
+            if (_moveing)
+            {
+                _moveVec = (_tragetPos - _myPos);
+                transform.position += _moveVec * Time.deltaTime * _moveSpeed;
+            }
+            else
+            {
+                _moveing = true;
+            }
+        }
+        else
+        {
+            _moveing = false;
+        }
     }
-
-
-
 }
