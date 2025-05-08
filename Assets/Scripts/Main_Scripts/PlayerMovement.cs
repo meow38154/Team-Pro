48eb49f8bcc0ecd3f8e50df27272c4303aee1e37
riwoo0902 <imliu4342@gmail.com>
2025-05-08 23:03:52 +0900

    save

--- Assets/Scripts/Main_Scripts/PlayerMovement.cs
+++ Assets/Scripts/Main_Scripts/PlayerMovement.cs
@@ -214,11 +214,7 @@ public class PlayerMovement : MonoBehaviour
 
     IEnumerator MoveCoolTime()
     {
-<<<<<<< HEAD
-        _movePossible = false;
-=======
         _movePossbie = false;
->>>>>>> parent of 4ef4e39 (Fixed: code)
         CtrlZManager.AllSave();
         yield return new WaitForSeconds(_moveCoolTIme);
         _movePossbie = true;
