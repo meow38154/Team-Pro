46c1096a58fccfad54931efc94ecc760251b33b1
meow38154 <jing76371@gmail.com>
2025-05-24 22:14:35 +0900

    fix: Menu

--- Assets/Scripts/CYS_Scripts/Buttons.cs
+++ Assets/Scripts/CYS_Scripts/Buttons.cs
@@ -11,8 +11,13 @@ namespace CYS_Scripts
         [FormerlySerializedAs("_turnOffTheLight")] [SerializeField] private GameObject turnOffTheLight;
         [FormerlySerializedAs("_dontDisturb")] [SerializeField] private GameObject dontDisturb;
         [SerializeField] private GameObject _3dCubeR;
-        [SerializeField] private GameObject _3dCuben;
+        [SerializeField] private GameObject Setting;
         [FormerlySerializedAs("_credit")][SerializeField] private GameObject creditForm; [FormerlySerializedAs("_quitting")][SerializeField] private GameObject quitting;
+        [SerializeField] GameObject _gold;
+        [SerializeField] GameObject _play;
+        [SerializeField] GameObject _logo;
+        [SerializeField] GameObject _ob;
+        [SerializeField] GameObject _ahPlz;
 
         bool _isTimeOver;
     
@@ -28,18 +33,24 @@ namespace CYS_Scripts
         public void StartingButton()
         {
             AudioManager.Instance.PlayUICilck();
-            //_3dCuben.SetActive(false);
+            _gold.GetComponent<RotionPlus>()._play = true;
+            _play.SetActive(false);
+            Setting.SetActive(false);
             dontDisturb.SetActive(false);
             turnOffTheLight.SetActive(false);
+            _ob.SetActive(false);
+            _logo.SetActive(false);
             startingForm.SetActive(true);
             _3dCubeR.SetActive(true);
             quitting.SetActive(false);
+            _ahPlz.GetComponent<MouseMove>()._start = true;
         }
 
       public void Credit()
         {
             AudioManager.Instance.PlayUICilck();
             creditForm.SetActive(true);
+            Setting.SetActive(false);
         }
         
         
--- Assets/Scripts/CYS_Scripts/CancelCredit.cs
+++ Assets/Scripts/CYS_Scripts/CancelCredit.cs
@@ -4,10 +4,13 @@ using UnityEngine.Serialization;
 public class CancelCredit : MonoBehaviour
 {
     [FormerlySerializedAs("_credit")][SerializeField] private GameObject creditForm;
+    [SerializeField] GameObject _s;
 
 
     public void Cancel()
     {
         creditForm.SetActive(false);
+        _s.SetActive(true);
+        AudioManager.Instance.PlayUICilck();
     }
 }
