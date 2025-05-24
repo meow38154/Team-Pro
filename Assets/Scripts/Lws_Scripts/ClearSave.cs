39e111d519000d1b231a208d2630e0aed4b10878
riwoo0902 <imliu4342@gmail.com>
2025-05-25 03:59:26 +0900

    fix:ClearSave

--- Assets/Scripts/Lws_Scripts/ClearSave.cs
+++ Assets/Scripts/Lws_Scripts/ClearSave.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class ClearSave : MonoBehaviour
 {
     private int _clearStage = 0;
 
-    SettingManagerr _ClearStageClass = SettingManagerr.Instance;
+    
 
     public static ClearSave Instance;
 
@@ -12,8 +14,7 @@ public class ClearSave : MonoBehaviour
     private int Stage2Open = 0;
     private int Stage3Open = 0;
 
-
-
+    private float currentTime = 0;
     private void Awake()
     {
         if (Instance == null)
@@ -41,19 +42,49 @@ public class ClearSave : MonoBehaviour
     private void Update()
     {
         Debug.Log($"{Stage1Open}, {Stage2Open}, {Stage3Open}");
+
+
+        openReset();
+
+
+        if(currentTime <= 0)
+        {
+            SettingManagerSetting(0);
+            Debug.Log("riwoo");
+            currentTime = 2;
+        }
+        else
+        {
+            currentTime -= Time.deltaTime;
+        }
     }
 
+
+    private void openReset()
+    {
+        if (Keyboard.current.rKey.isPressed && Keyboard.current.numpad0Key.wasPressedThisFrame)
+        {
+
+            PlayerPrefs.SetInt("Stage1Open", 0);
+            PlayerPrefs.SetInt("Stage2Open", 0);
+            PlayerPrefs.SetInt("Stage3Open", 0);
+            SettingManagerr.Instance.Stage1Open = false;
+            SettingManagerr.Instance.Stage2Open = false;
+            SettingManagerr.Instance.Stage3Open = false;
+        }
+    }
 
     public void SettingManagerSetting(int a)
     {
         if(a != 0)PlayerPrefs.SetInt($"Stage{a}Open",1);
-        PlayerPrefs.SetInt("Stage1Open", Stage1Open);
-        PlayerPrefs.SetInt("Stage2Open", Stage2Open);
-        PlayerPrefs.SetInt("Stage3Open", Stage3Open);
 
-        _ClearStageClass.Stage1Open = Stage1Open == 1 ? true : false;
-        _ClearStageClass.Stage2Open = Stage2Open == 1 ? true : false;
-        _ClearStageClass.Stage3Open = Stage3Open == 1 ? true : false;
+        Stage1Open = PlayerPrefs.GetInt("Stage1Open",0);
+        Stage2Open = PlayerPrefs.GetInt("Stage2Open",0);
+        Stage3Open = PlayerPrefs.GetInt("Stage3Open",0);
+
+        SettingManagerr.Instance.Stage1Open = Stage1Open == 1 ? true : false;
+        SettingManagerr.Instance.Stage2Open = Stage2Open == 1 ? true : false;
+        SettingManagerr.Instance.Stage3Open = Stage3Open == 1 ? true : false;
     }
 
     public void GetMetal(int a,int b,int c,int d)
@@ -64,9 +95,9 @@ public class ClearSave : MonoBehaviour
         PlayerPrefs.SetInt("Gold", PlayerPrefs.GetInt("Gold") + a);
         PlayerPrefs.SetInt("HapGold", PlayerPrefs.GetInt("HapGold") + a);
 
-        _ClearStageClass.Copper  = PlayerPrefs.GetInt("Copper");
-        _ClearStageClass.Iron = PlayerPrefs.GetInt("Iron");
-        _ClearStageClass.Gold = PlayerPrefs.GetInt("Gold");
-        _ClearStageClass.HapGold = PlayerPrefs.GetInt("HapGold");
+        SettingManagerr.Instance.Copper  = PlayerPrefs.GetInt("Copper");
+        SettingManagerr.Instance.Iron = PlayerPrefs.GetInt("Iron");
+        SettingManagerr.Instance.Gold = PlayerPrefs.GetInt("Gold");
+        SettingManagerr.Instance.HapGold = PlayerPrefs.GetInt("HapGold");
     }
 }
