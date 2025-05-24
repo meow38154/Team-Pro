304bda6fb844270596142ec58c83500e2ad61238
riwoo0902 <imliu4342@gmail.com>
2025-05-25 01:05:50 +0900

    feat:ClearSave

---
+++ Assets/Scripts/Lws_Scripts/ClearSave.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class ClearSave : MonoBehaviour
+{
+    private int _clearStage = 0;
+
+    public int ClearStage
+    {
+        get
+        {
+            return _clearStage;
+        }
+        set
+        {
+            _clearStage = value;
+            setClearStage();
+        }
+    }
+
+    private void Start()
+    {
+        ClearStage = PlayerPrefs.GetInt("ClearStage");
+    }
+
+
+    private void setClearStage()
+    {
+
+        PlayerPrefs.SetInt("ClearStage", ClearStage);
+
+        //int a = PlayerPrefs.GetInt("ClearStage");
+        //if(ClearStage > a)
+        //{
+        //    PlayerPrefs.SetInt("ClearStage", ClearStage);
+        //}
+        //else if(ClearStage < a) 
+        //{
+        //    ClearStage = a;
+        //}
+    }
+
+    private void SettingManagerSetting()
+    {
+        SettingManager a = SettingManager.Instance;
+        bool[] b = { a.Stage1Open, a.Stage2Open, a.Stage3Open };
+        //for(int i = 0;i < ClearStage;)
+    }
+}
